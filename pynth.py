21afad9aec84747c5fbac440bea763cd15f4131e
MatejBevec <matejbevec98@gmail.com>
2023-01-02 21:47:19 +0100

    filters and examples

--- pynth.py
+++ pynth.py
@@ -19,7 +19,7 @@ from graphviz import Digraph
 import keyboard
 
 SR = 48000 # TEMP 
-CHUNK = 100 # TIME CHUNK LEN IN SAMPLES
+CHUNK = 200 # TIME CHUNK LEN IN SAMPLES
 MAXDEPTH = 30
 gtime = 0 # CURRENT CHUNK ON THE CLOCK - TEMP
 globnodes = []
@@ -77,6 +77,8 @@ anim = None # necessary matplotlib thing
 # TODO: stateful modules need to have some sort of _reset function that gets
 # on every new eval() call
 
+# TODO: how to handle multiple outputs on modules like sequencer/piano roll
+
 
 def toposort(out):
     topo = []
@@ -133,6 +135,40 @@ def _showscopes():
     plt.show()
 
 
+def get_crossings(a, lastprev=0, thr=0.5):
+    a = np.insert(a, 0, lastprev)
+    mask = (a > thr)
+    shifted = np.zeros(len(mask)).astype(bool)
+    shifted[1:] = mask[:-1]
+    imp = np.logical_and(mask, np.logical_not(shifted))
+    imp = imp[1:]
+    loc = np.nonzero(imp)[0]
+    return loc
+
+def clamp_signal(a, range=[0.001, 0.999]):
+    print(a)
+    a = np.array(a)
+    inmin = np.min(a)
+    inrange = np.max(a) - np.min(a)
+    print(inmin, inrange)
+    a -= inmin; a /= inrange
+    print(a)
+    outrange = range[1] - range[0]
+    a *= outrange; a += range[0]
+    print(a)
+    return a
+
+def to_unipolar(a):
+    """From [-1, 1] to [0, 1]"""
+    a = np.clip(np.array(a), -1, 1)
+    return (a + 1) / 2
+
+def to_bipolar(a):
+    """From [0, 1] to [-1, 1]"""
+    a = np.clip(np.array(a), 0, 1)
+    return a * 2 - 1
+
+
 class Module():
     """Base class"""
 
@@ -152,7 +188,7 @@ class Module():
         #return f"{self.i}: {self.__class__.__name__}"
         return f"{self.__class__.__name__}({self.i})"
 
-    def _compute(self, t):
+    def _compute(self, t, d=0):
         """Compute the next chunk.
         
         Args:
@@ -339,17 +375,7 @@ class Wave(Module):
         return self._getdata(tsec, d)
 
 
-def get_crossings(a, lastprev=0):
-    a = np.insert(a, 0, lastprev)
-    mask = (a > 0.5)
-    #print(mask)
-    shifted = np.zeros(len(mask)).astype(bool)
-    shifted[1:] = mask[:-1]
-    #print(shifted)
-    imp = np.logical_and(mask, np.logical_not(shifted))
-    imp = imp[1:]
-    loc = np.nonzero(imp)[0]
-    return loc
+
 
 
 # TODO: a better user intf would be to have a method like
@@ -476,19 +502,24 @@ class Sequencer(Wave):
 
     def __init__(self, ina, sequence):
         self.ins = {"a": ina}
+        if isinstance(sequence, list):
+            sequence = np.array(sequence)
         self.sequence = sequence
         self.state = 0
+        self.lastprev = 1
         super().__init__(None)
 
     def data(self, t):
         a = self.indata["a"]
         out = np.zeros(len(t))
-        loc = get_crossings(a)
+        loc = get_crossings(a, lastprev=self.lastprev)
+        self.lastprev = a[-1]
         prev = 0
         for i in loc:
             out[prev:i] = float(self.sequence[self.state])
             out[i-1] = 0.0
-            self.state = (self.state + 1) % len(self.sequence)
+            self.state += 1
+            if self.state == len(self.sequence): self.state = 0
             prev = i
         out[prev:] = float(self.sequence[self.state])
         return out
@@ -535,6 +566,12 @@ class Square(Wave):
         self.amp = amp
         self.data = lambda t: self.amp * np.sign(np.sin(t*2*math.pi*self.freq))
 
+class WhiteNoise(Wave):
+
+    def __init__(self):
+        super().__init__(None)
+        self.data = lambda t: np.random.rand(len(t))*2 - 1
+
 
 # --- OPERATIONS ---
 
@@ -578,11 +615,31 @@ class Compare(TwoOp):
 
 
 
+class Unipol(Module):
+    """Transform bipolar [-1, 1] signal to unipolar [0, 1]"""
+
+    def __init__(self, ina=None):
+        self.ins = {"a": ina}
+        super().__init__()
+    
+    def _compute(self, t, d=0):
+        return to_unipolar(self.indata["a"])
+
+class Bipol(Module):
+    """Transform unipolar [0, 1] signal to bipolar [-1, 1]"""
+
+    def __init__(self, ina=None):
+        self.ins = {"a": ina}
+        super().__init__()
+    
+    def _compute(self, t, d=0):
+        return to_bipolar(self.indata["a"])
+
+
 class Delay(Module):
     """Output a delayed input signal"""
 
     def __init__(self, ina=None, delay=0.005):
-       
         self.ins = {"a": ina}
         self.delay = delay
         dsamp = int(delay * SR)
@@ -598,38 +655,6 @@ class Delay(Module):
         self.zi = zf
         return y
 
-class Lowpass(Module):
-    """Moving average lowpass filter"""
-
-    def __init__(self, ina=None, incontrol=None, cutoff=500):
-        self.ins = {"a": ina, "control": incontrol}
-        self.cutoff = cutoff
-        M = int(SR/cutoff)
-        self.b = np.ones(M)/M
-        self.a = [1]
-        self.zi = np.zeros(M-1)
-        super().__init__()
-
-    def setparam(self):
-        control = self.indata["control"]
-        maxcut = 5000
-        mincut = 500
-        cutoff = control[0]*maxcut + (1-control[0])*mincut
-        M = int(SR/cutoff)
-        b = np.ones(M)/M
-        zi = self.zi[-(M-1):]
-        topad = max(M-1 - len(zi), 0)
-        zi = np.pad(zi, (topad, 0))
-        self.zi = zi
-        self.b = b
-
-    def _compute(self, t, d=0):
-        if self.ins["control"] is not None:
-            self.setparam()
-        x = self.indata["a"]
-        y, zf = sp.signal.lfilter(self.b, self.a, x, zi=self.zi)
-        self.zi = zf
-        return y
 
 
 class FreqFilter(Module):
@@ -653,40 +678,106 @@ class FreqFilter(Module):
         # 7. store next buffer
         pass
 
-# TEMP
-class Butter(Module):
+class Filter(Module):
 
-    def __init__(self, ina=None, crit=100, btype="lowpass", fs=SR):
-        self.ins = {"a": ina}
-        self.crit = 0.1
-        self.btype = btype
-
-        order = 4
-        self.b, self.a = sp.signal.butter(order, self.crit, self.btype, analog=False)
-        
-        #w = w * SR / 2
-
-        freq = self.crit*SR / 2
-        b, a = sp.signal.butter(order, freq, self.btype, analog=True)
-        w, h = sp.signal.freqs(b, a)
-        plt.semilogx(w, 20 * np.log10(abs(h)))
-        plt.title('Butterworth filter frequency response')
-        plt.xlabel('Frequency [radians / second]')
-        plt.ylabel('Amplitude [dB]')
-        plt.margins(0, 0.1)
-        plt.grid(which='both', axis='both')
-        plt.show()
-
+    def __init__(self):
+        # A filter should set self.a and self.b at init
+        # and expose _getparam which returns new a, b according to control signal
+        print(self.a)
         self.zi = sp.signal.lfilter_zi(self.b, self.a)
         super().__init__()
 
+    def _setparam(self):
+        if self._getparam is None: return
+        param = self._getparam()
+        if param is None: return
+        self.b, self.a = param
+
+        znew = sp.signal.lfilter_zi(self.b, self.a)
+        if len(self.zi) > len(znew):
+            self.zi = self.zi[-len(znew):]
+        else:
+            znew[:len(self.zi)] = self.zi
+            self.zi = znew
+        
     def _compute(self, t, d=0):
-        #print(self.__dict__)
+        self._setparam()
         x = self.indata["a"]
         y, zf = sp.signal.lfilter(self.b, self.a, x, zi=self.zi)
         self.zi = zf
         return y
 
+# TODO: generalize filters and modulation
+
+class Butter(Filter):
+
+    # BUG: impulse artifact
+
+    def __init__(self, ina=None, incontrol=None, crit=0.3, btype="lowpass", fs=SR):
+        self.ins = {"a": ina, "control": incontrol}
+        self.crit = crit
+        self.btype = btype
+        self.b, self.a = sp.signal.butter(2, self.crit, self.btype, analog=False)
+        super().__init__()
+
+    def _getparam(self):
+        if self.ins["control"] is None: return
+        # TODO: cap at [0, 1]
+        crit = self.indata["control"][0]
+        assert crit >= 0 and crit <= 1
+        #print(crit)
+        b, a = sp.signal.butter(2, crit*0.99+0.005, self.btype, analog=False)
+        return b, a
+
+class MovingAvg(Filter):
+    
+    def __init__(self, ina=None, incontrol=None, M=50):
+        self.ins = {"a": ina, "control": incontrol}
+        self.M = M
+        self.b, self.a = np.ones(M)/M, [1]
+        super().__init__()
+
+    def _getparam(self):
+        if self.ins["control"] is None: return
+        val = self.indata["control"][0]
+        assert val  >= 0 and val <= 1
+        M = int((1-val)*self.M)
+        b, a = np.ones(M)/M, [1, 0]
+        return b, a
+
+class EQ(Filter):
+
+    def __init__(self, ina=None, freq=[20, 20000], gain=[1, 1]):
+        self.ins = {"a": ina}
+        self.b, self.a = sp.signal.firwin2(100, freq, gain)
+        super().__init__()
+
+class Lowpass(Filter):
+
+    def __init__(self, ina=None, incontrol=None, inres=None, cutoff=0.5, res=0.2):
+        self.ins = {"a": ina, "control": incontrol, "res": inres}
+        self.cutoff = cutoff
+        self.res = res
+        self.b, self.a = self._getfilt(cutoff, res)
+        super().__init__()
+
+    def _getfilt(self, cutoff, res):
+        cband = 0.2 # width of cutoff transition
+        rband = 0.05 # width of transition to resonance peak
+        cutoff = max(min(0.99, cutoff), 0.01)
+        freq = [0, max(cutoff-rband, 0.01), cutoff, min(cutoff+cband, 0.99), 1]
+        #print(freq)
+        gain = [1-res, 1-res, 1, 0, 0]
+        return sp.signal.firwin2(100, freq, gain), [1, 0]
+
+    def _getparam(self):
+        inc = self.ins["control"] is not None
+        inr = self.ins["res"] is not None
+        #print(self.indata["control"])
+        if not (inc or inr): return
+        if inc: self.cutoff = self.indata["control"][0]
+        if inr: self.res = self.indata["res"][0]
+        return self._getfilt(self.cutoff, self.res)
 
 
 
@@ -738,16 +829,16 @@ Crosstrigger = compose( lambda a:  (a > 0) * (((a > 0) >> 1/SR) < 0.5) , "Crosst
 
 # --- VISUALIZATION ---
     
-def showsound(module, x1=0, x2=30000, sec=False):
+def showsound(module, t1=0, t2=30000, sec=False):
     if sec:
-        x1, x2 = int(x1*SR), int(x2*SR)
-    data = module.eval(x2)
-    x = np.linspace(0, x2, x2)
+        t1, t2 = int(t1*SR), int(t2*SR)
+    data = module.eval(t2)
+    x = np.linspace(0, t2, t2)
     if sec:
         x /= SR
-        x1, x2 = x1/SR, x2/SR
+        t1, t2 = t1/SR, t2/SR
     plt.plot(x, data)
-    plt.xlim((x1, x2))
+    plt.xlim((t1, t2))
     plt.show()
 
 
@@ -854,46 +945,40 @@ def drawgraph(root, format='svg', rankdir='LR', render=True):
 
 if __name__ == "__main__":
 
-    # MUSIC?
+    # OVERTONES
 
-    # gate = Wave(np.linspace(-1, 1, 50000))
-
-    # impulses = Pulses(1/SR, 0.5)
-
-    # ramp = Ramp((0, 16)) ** 2
-    # hum = Sin(100) * ramp * 0.3
-
-    # env = Envelope(impulses >> 0.01, durs=(3000, 0, 0, 12000))
-    # voice = Square(50) + Saw(50)
-
-    # out = env * voice +hum
-
-    # mask = Pulse((8, 24))
-    # voice2 = Sin(1000) * Sin(4) >> 0.01
+    # out = 0.5 * Sin(200)
+    # for i in range(5):
+    #     out += 0.1 * Sin(100*i)
     
-    # out += mask * voice2
-
-    # ramp2 = Ramp((8, 16)) ** 2
-    # out += Sin(30) * ramp2
-
-    # mask2 = Pulse((24, 40))
-    # env2 = Envelope(impulses >> 0.011, durs=(1000, 0, 0, 6000))
-    # newkick = Sin(50)*2 * env2 * mask2
-    # out = 0.7*out + newkick + Sin(4000)*0.2 * mask2
+    # drawgraph(out); showsound(out, t2=0.5*SR)
+    # out.play(5*SR)
 
 
-    # FILTER ENVELOPE
+    # SAMPLES, VOCAL REMOVER
 
-    # impulses = Pulses(1/SR, 1)
-    # env = Envelope(impulses, durs=(3000, 0, 0, 12000))
-    # voice = Saw(200)
-    # lowpass = Lowpass(voice, env)
+    # sample, sr = librosa.load("Phlex_short.wav", mono=False)
+    # l = Wave(sample[0, :])
+    # r = Wave(sample[1, :])
+    # SR = sr
 
-    # print("hello")
-    # ramp = Wave(np.linspace(0, 1, 50000))
-    # print("world")
-    # out = Lowpass(Saw(200), ramp)
-    # print("fasfdas")
+    # out = 0.5 * (0.5*l - 0.5*r) + 0.5 * MovingAvg(0.5*l + 0.5*r, M=50)
+
+    # drawgraph(out)
+    # out.play(10*SR)
+
+    
+    # KARPLUS, FREEZING SAMPLES
+
+    # noise = Wave(np.random.rand(4000)-0.5)
+    # delay = Delay(None, delay=0.005)
+    # add = noise + delay
+    # delay.ins["a"] = 0.95 * MovingAvg(add, M=10)
+    # out = add
+    # frozen = Wave(add.eval(2*SR))
+
+    # drawgraph(out); showsound(frozen, t2=2*SR)
+    # out.play(2*SR)
 
 
     # SEQUENCER
@@ -903,53 +988,14 @@ if __name__ == "__main__":
     # env = Envelope(seq, durs=(1000, 0, 0, 6000))
 
     # out = env * Saw(200) * Sin(50)
-    # out = Highpass(out)
-
-    # VOCAL REMOVER
-
-    # sample, sr = librosa.load("Phlex_short.wav", mono=False)
-    # l = Wave(sample[0, :])
-    # r = Wave(sample[1, :])
-    # SR = sr
-
-    # out = 0.5 * (0.5*l - 0.5*r) + 0.5 * Lowpass(0.5*l + 0.5*r, cutoff=100)
-
-    
-    # OVERTONES
-
-    # out = 0.5 * Sin(200)
-    # for i in range(5):
-    #     out += 0.1 * Sin(100*i)
-
-    # KARPLUS
-
-    # noise = Wave(np.random.rand(4000)-0.5)
-    # delay = Delay(None, delay=0.005)
-    # add = noise + delay
-    # delay.ins["a"] = 0.93 * add
-    # out = add
-    # frozen = out.eval(2*SR)
-
-    # sample, sr = librosa.load("Phlex_short.wav", mono=False)
-    # l = Scope(Wave(sample[0, :]))
-    # r = Scope(Wave(sample[1, :]))
-    # SR = sr
-
-    # out = 0.5 * (0.5*l - 0.5*r) + 0.5 * Lowpass(0.5*l + 0.5*r, cutoff=100)
-    # out = Scope(out)
-
-    # g = drawgraph(out); g.render("gout", view=True)
-    # out.play(100000, live=False)
 
 
-    # ENVELOPE INPUT
-    # SCOPERATE = 8
-    # CHUNK = 300
 
+    # ENVELOPES, USER INPUT, SCOPES
     # control = Input()
-    # env = Envelope(Scope(control, window=0.5), (2000, 0, 0, 8000))
-    # out = Scope(env, window=0.5)
-    # out = Scope(out * Saw(100), window=0.5)
+    # env = Envelope(Scope(control), (2000, 0, 0, 8000))
+    # out = Scope(env)
+    # out = Scope(out * Saw(100))
 
     # #global val
     # val = 0
@@ -963,29 +1009,40 @@ if __name__ == "__main__":
     # out.play(30*SR, live=True, callback=loop)
 
 
-    # # LOWPASS TESTS
-    # sample, sr = librosa.load("house.mp3", mono=False)
+    # LOWPASS SWIPE
+    # sample, sr = librosa.load("house.mp3", mono=True)
     # SR = sr
-    # music = Scope(Wave(sample[0, :]))
-    # out = Scope(Lowpass(music))
-    # drawgraph(out)
-    # out.play(180*SR, live=True)
+    # music = Wave(sample)    
+    # control = Scope(Sin(1/5)*0.5+0.5)
+    # out = Lowpass(music, control, res=0.4)
+    # out = Scope(out)
+    # out.play(20*SR, live=True, callback=None)
+
+
+    # PROCEDURAL WIND
+    noise = WhiteNoise()
+    cutmod = Unipol(0.5*Sin(1/3) + 0.5*Sin(1/5)) * 0.1
+    resmod = Unipol((0.5*Sin(1) + 0.5*Sin(0.8)) >> 0.3) * 0.4 + 0.1
+    out = Scope(Lowpass(noise, cutmod, resmod))
+    drawgraph(out)
+    out.play(30*SR, live=False)
+
 
     
-    # FILTER TESTS
-    sample, sr = librosa.load("house.mp3")
-    SR = sr
-    music = Scope(Wave(sample))
-    butter = Butter(music, crit=500, btype="low")
-    out = Scope(butter)
-    out.play(60*SR, live=True)
+    # THIS IS ACID MAAAN
+    # clock = Pulses(T=1/8)
+    # clock = Sequencer(clock, [0,1,0,1,0,1,1,1])
+    # env = Scope(Envelope(clock, durs=(1000, 0, 0, 5000)))
+    # voice = Saw(80)
+    # out = env * Lowpass(voice, env, res=0.6)
+    # out = Scope(out)
+    # t0 = time.time()
+    # out.play(20*SR, live=False)
+    # print(time.time() - t0)
 
-
-    #g = drawgraph(out); g.render("gout", view=True)
-    #showsound(out, x2=150000/SR, sec=True)
-    #out.play(150000, live=True, callback=loop)
 
     # TODO: should samples or seconds be the standard unit of time ?
     # TODO: should the interface instead be Module(params)(inputs)
-
+    # TODO: cap outputs at [-1, 1] ?
+    # TODO: kill all threads if main crashes
 
